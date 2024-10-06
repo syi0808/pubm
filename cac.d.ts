ffb7ff550059803f715b8386536e698d800be1b3
syi0808 <syi9397@naver.com>
2024-10-07 00:06:26 +0900

    formatting

--- cac.d.ts
+++ cac.d.ts
@@ -1,7 +1,7 @@
 declare module 'cac/deno/Option.js' {
 	interface OptionConfig {
 		default?: unknown;
-		type?: ((value: unknown) => unknown) | any[];
+		type?: ((value: unknown) => unknown) | unknown[];
 	}
 
 	export type { OptionConfig };
