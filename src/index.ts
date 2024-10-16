bb0c5d26f5fa38469663dac94f77a8eccd5ee736
syi0808 <syi9397@naver.com>
2024-10-16 12:38:36 +0900

    docs: symbol docs

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,23 @@ import { resolveOptions } from './options.js';
 import { run } from './tasks/runner.js';
 import type { Options } from './types/options.js';
 
+/**
+ * Runs the `pubm` function with the provided options.
+ *
+ * This function executes the publish process using the specified options.
+ * The `version` field in the `options` parameter is required for the function
+ * to run correctly.
+ *
+ * @async
+ * @function
+ */
 export async function pubm(options: Options) {
 	const resolvedOptions = resolveOptions({ ...options });
 
 	await run(resolvedOptions);
 }
+
+/**
+ * Options for configuring the {@linkcode pubm} function.
+ */
+export type { Options } from './types/options.js';
