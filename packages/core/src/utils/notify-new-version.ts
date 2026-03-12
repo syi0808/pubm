5b1ff46b35053243337f1b52a0ecb431cc612016
syi0808 <syi9397@naver.com>
2026-03-12 11:38:27 +0900

    Fix update-kit package identity resolution

--- packages/core/src/utils/notify-new-version.ts
+++ packages/core/src/utils/notify-new-version.ts
@@ -1,7 +1,10 @@
 import { UpdateKit } from "update-kit";
+import { PUBM_VERSION } from "./pubm-metadata.js";
 
 export async function notifyNewVersion(): Promise<void> {
   const kit = await UpdateKit.create({
+    appName: "pubm",
+    currentVersion: PUBM_VERSION,
     sources: [{ type: "npm", packageName: "pubm" }],
   });
 
