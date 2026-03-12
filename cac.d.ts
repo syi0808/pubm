1789b8fb98deef14352bfb82db21aa9ac8aa9aa8
syi0808 <syi9397@naver.com>
2026-03-12 18:53:39 +0900

    chore: remove cac.d.ts

--- cac.d.ts
+++
@@ -1,8 +0,0 @@
-declare module "cac/deno/Option.js" {
-  interface OptionConfig {
-    default?: unknown;
-    type?: ((value: unknown) => unknown) | unknown[];
-  }
-
-  export type { OptionConfig };
-}
